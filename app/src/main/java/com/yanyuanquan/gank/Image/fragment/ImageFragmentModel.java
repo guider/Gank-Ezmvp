6cac727648da43893affca72c459db009a5b4d3c
guider <guider@yeah.net>
2016-06-24 15:21:37 +0800

    no message

---
+++ app/src/main/java/com/yanyuanquan/gank/Image/fragment/ImageFragmentModel.java
@@ -0,0 +1,11 @@
+package com.yanyuanquan.gank.Image.fragment;
+
+import com.yanyuanquan.android.automvp.model.BaseModel;
+
+/**
+ * Created by guider on 16/6/24.
+ * Email guider@yeah.net
+ * github https://github.com/guider
+ */
+public class ImageFragmentModel extends BaseModel {
+}
